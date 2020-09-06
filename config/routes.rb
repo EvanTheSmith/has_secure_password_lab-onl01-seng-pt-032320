0d49f524dc39e09b7c7e9bc9cd63cf44ae402321
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-09-06 02:39:17 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'        # Sign Up Form
   post '/signup' => 'users#create'    # Sign Up Post Path
 
-  get '/login' => 'sessions#create'  # Log In Form
+  get '/login' => 'sessions#new'  # Log In Form
   post '/login' => 'sessions#create'  # Log In Post Path
 
   root 'welcome#home'
